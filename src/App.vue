74b0bf3f837034570e46d15b14f3433fa36220e3
fff <aaabbbccc@qq.com>
2020-03-24 20:56:00 +0800

    App.vue设置公共样式

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,29 @@
   </div>
 </template>
 <style lang="less">
+// 公共的样式
+* {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+a:hover {
+  color: inherit;
+}
+
+ul,
+li,
+ol {
+  list-style: none;
+}
+
+i,
+em {
+  font-style: normal;
+}
 </style>
